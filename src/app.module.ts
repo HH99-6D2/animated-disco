fa0c19dff28baaa2e2cf280c584a0c3b2374d08c
junehan <junehan.dev@gmail.com>
2022-05-02 17:12:48 +0900

    INIT/Builtin Resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+//import { config } from "./ormconfig";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+//		TypeOrmModule.forRoot(config),
+		ConfigModule.forRoot({ envFilePath: '.development.env' }),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
