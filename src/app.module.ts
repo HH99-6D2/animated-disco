d1909d370541864554f99a485d1a09f410b32faf
junehan <junehan.dev@gmail.com>
2022-05-05 10:51:06 +0900

    [INIT]: User Module Initiated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 import config from 'ormconfig';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config),
     ConfigModule.forRoot({ envFilePath: '.development.env' }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
