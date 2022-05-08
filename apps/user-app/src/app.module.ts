90e7f3f4bdd518bb217670c6815c56de75080b6c
junehan <junehan.dev@gmail.com>
2022-05-08 21:10:28 +0900

    [INIT] User-app/users module initialized

--- apps/user-app/src/app.module.ts
+++ apps/user-app/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserAppController } from './app.controller';
 import { UserAppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [UserAppController],
   providers: [UserAppService],
 })
