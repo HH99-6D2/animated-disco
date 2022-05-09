51244fc2c8c07af78de2a4cdde143b618cb815a5
junehan <junehan.dev@gmail.com>
2022-05-09 17:50:29 +0900

    [INIT] Auth module Initialized

--- apps/user-app/src/app.module.ts
+++ apps/user-app/src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 //import { UserAppService } from './app.service';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { UsersModule } from './users/users.module';
       },
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [
     //   UserAppController
