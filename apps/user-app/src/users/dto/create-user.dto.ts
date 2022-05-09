b36c1a6f94f1cd7695937d056aac27dd951f2258
junehan <junehan.dev@gmail.com>
2022-05-09 12:56:04 +0900

    [FEAT]NEW/User CreateDTO Created

--- apps/user-app/src/users/dto/create-user.dto.ts
+++ apps/user-app/src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description:
+      'An id from SocialProfile of a user which is already generated before create user.',
+  })
+  @IsNumber()
+  id: number;
+
+  @ApiProperty({
+    description: 'NickName of user which is temporary.',
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(20)
+  nickname: string;
+}
--- apps/user-app/src/users/entities/user.entity.ts
+++ apps/user-app/src/users/entities/user.entity.ts
@@ -4,7 +4,7 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 export class User {
   @PrimaryColumn({ nullable: false })
   id: number;
-  @Column({ type: 'varchar', length: 60 })
+  @Column({ type: 'varchar', length: 20 })
   nickname: string;
   @Column({ type: 'boolean', default: true })
   isActive: boolean;
