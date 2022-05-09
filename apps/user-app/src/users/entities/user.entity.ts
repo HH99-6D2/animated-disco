30e80f8fa52ccd51a6460855f903a0cd9134ce07
junehan <junehan.dev@gmail.com>
2022-05-09 12:50:24 +0900

    [FEAT]NEW/User Entity Initialized

--- apps/user-app/src/users/entities/user.entity.ts
+++ apps/user-app/src/users/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn({ nullable: false })
+  id: number;
+  @Column({ type: 'varchar', length: 60 })
+  nickname: string;
+  @Column({ type: 'boolean', default: true })
+  isActive: boolean;
+}
