5325ce516044298ade92a6fbe964b47f08a8e3ec
yogoloper <jguddnjs@gmail.com>
2022-05-10 23:28:31 +0900

    [CHORE] room creation data changed

--- apps/room-app/src/rooms/rooms.service.ts
+++ apps/room-app/src/rooms/rooms.service.ts
@@ -6,6 +6,7 @@ import { RoomRepository } from './room.repository';
 import { TagRepository } from '../tags/tag.repository';
 import { Tag } from '../tags/entities/tag.entity';
 import { create } from 'domain';
+import { Not } from 'typeorm';
 
 @Injectable()
 export class RoomsService {
@@ -16,40 +17,21 @@ export class RoomsService {
   ) {}
 
   async getAllRooms(): Promise<Room[]> {
-    const rooms = await this.roomRepository.find();
+    const rooms = await this.roomRepository.find({
+      status: Not(2),
+    });
     return rooms;
   }
 
   async createRoom(createRoomDto: CreateRoomDto): Promise<void> {
-    const {
-      title,
-      positionX,
-      positionY,
-      startDate,
-      endDate,
-      categoryId,
-      maxUser,
-      imageUrl,
-      regionAId,
-      regionBId,
-      tags,
-    } = createRoomDto;
+    const { tags } = createRoomDto;
 
     const roomTags = await this.tagRepository.findOrInsert(tags);
 
     const room = this.roomRepository.create({
+      ...createRoomDto,
       userId: 1,
-      title,
-      positionX,
-      positionY,
-      startDate,
-      endDate,
-      categoryId,
-      maxUser,
-      imageUrl,
-      regionAId,
-      regionBId,
-      status: new Date(startDate) <= new Date() ? 1 : 0,
+      status: new Date(createRoomDto.startDate) <= new Date() ? 1 : 0,
       tags: roomTags,
     });
 
