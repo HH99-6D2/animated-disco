c2cbb3c1157e3f1b0d98f53b89aa4fe97117f1b3
yogoloper <jguddnjs@gmail.com>
2022-05-11 23:13:50 +0900

    [CHORE] database config .env added

--- apps/room-app/src/configs/typeorm.config.ts
+++ apps/room-app/src/configs/typeorm.config.ts
@@ -2,14 +2,17 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Room } from '../entities/room.entity';
 import { Tag } from '../entities/tag.entity';
 import { Like } from '../entities/like.entity';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot({ envFilePath: '.room.development.env' })
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '1234',
-  database: 'room',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   // entities: [__dirname + '/../**/*.entity{.js,.ts}'],
   entities: [Room, Tag, Like],
   synchronize: true,
