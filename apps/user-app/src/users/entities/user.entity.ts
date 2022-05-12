5d45f151fad1a6aee442400617320838b491bcd4
junehan <junehan.dev@gmail.com>
2022-05-13 02:45:26 +0900

    [FIX] DB Scheme Change/DELETED/Auth -1 , User - 1
    - Auth.email
    - user.image

--- apps/user-app/src/users/entities/user.entity.ts
+++ apps/user-app/src/users/entities/user.entity.ts
@@ -6,8 +6,6 @@ export class User {
   id: number;
   @Column({ type: 'varchar', length: 16, nullable: true })
   nickname: string;
-  @Column({ type: 'varchar', length: 128, nullable: true })
-  imageUrl: string;
   @Column({ type: 'boolean', default: true, nullable: false })
   isActive: boolean;
 }
